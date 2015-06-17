73d2ff73ed10e6456b318923e4b111554f9a574b
methodswithclass <chris@methodswithclass.com>
2015-06-17 13:53:19 -0400

    html

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,4 @@
-app.controller('MainController', ['$scope', '$sce', function ($scope, $sce) {
+app.controller('MainController', ['$scope', '$sce', '$http', function ($scope, $sce, $http) {
 
 	$scope.hello = "hello World";
 
