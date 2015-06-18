0653b2fcd1afe774dad35bedf1ee1ae6bfa79e9f
methodswithclass <chris@methodswithclass.com>
2015-06-18 11:32:32 -0400

    menu click

--- js/directives/menu.js
+++ js/directives/menu.js
@@ -9,14 +9,16 @@ app.directive('menu', ['global', function (global) {
 	}
 			
 	var link = function (scope, element, attr) {
+		
+		element.on("click", function () {
 
-		if (element.id == "projectBtn") {
-			move(0);
-		}
-		else if (element.id == "contactBtn") {
-			move("-100%");
-		}
-
+			if (element.id == "projectBtn") {
+				move(0);
+			}
+			else if (element.id == "contactBtn") {
+				move("-100%");
+			}
+		});
 	}
 
 	return {
