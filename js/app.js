f4ee768f91b12107d46b3d1c4a4361987b254dd9
methodswithclass <chris@methodswithclass.com>
2015-06-18 00:29:58 -0400

    parallax

--- js/app.js
+++ js/app.js
@@ -1,18 +1,1 @@
 var app = angular.module("methodswithclass", []);
-
-app.run(function ($scope, parallax) {
-
-	parallax.scroll($scope.projects)
-
-	$("#projects").scroll(function () {
-		
-		parallax.scroll($scope.projects);
-
-	});
-
-	$(window).resize(function () {
-
-		parallax.scroll($scope.projects);
-	});
-
-});
---
+++ js/directives/scroll.js
@@ -0,0 +1,21 @@
+app.directive('scroll', ['$scope', 'parallax', function ($scope, parallax) {
+
+	return {
+		link:function () {
+
+			var scroll = function () {
+
+				parallax.scroll($scope.projects);
+
+			}
+
+			scroll();
+
+			$("#projects").scroll(scroll);
+
+			$(window).resize(scroll);
+
+		}
+	};
+
+});
