2ba12b7252e390fcab95623d34651fc8060fdff9
methodswithclass <chris@methodswithclass.com>
2015-06-18 11:57:29 -0400

    contact

--- js/directives/contact.js
+++ js/directives/contact.js
@@ -1,11 +1,15 @@
-app.directive('contact', function () {
+app.directive('contact', ['global', function (global) {
 	return {
-
 		restrict:'E',
 		scope:{
 			info:'='
 		},
-		templateUrl:'/views/contact.html'
+		templateUrl:'/views/contact.html',
+		link:function (scope, element, attr) {
+
+			scope.renderHtml = global.renderHtml;
+
+		}
 	};
 
-});
+}]);
