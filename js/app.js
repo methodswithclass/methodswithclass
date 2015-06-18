d51a66e473de96f56f468d5fd5794041b16af776
methodswithclass <chris@methodswithclass.com>
2015-06-18 00:12:16 -0400

    parallax

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 var app = angular.module("methodswithclass", []);
+
+app.run(function ($scope, parallax) {
+
+	parallax.scroll($scope.projects)
+
+	$("#projects").scroll(function () {
+		
+		parallax.scroll($scope.projects);
+
+	});
+
+	$(window).resize(function () {
+
+		parallax.scroll($scope.projects);
+	});
+
+});
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,7 @@
-app.controller('MainController', ['$scope', 'parallax', function ($scope, parallax) {
+app.controller('MainController', ['$scope', function ($scope) {
 
-	var apps = [
+
+	$scope.projects = [
 		{
 			id:'nuplae',
 			title:'nuplae',
@@ -27,31 +28,10 @@ app.controller('MainController', ['$scope', 'parallax', function ($scope, parall
 			maxSep:700
 	}];
 
-	var contact = {
+	$scope.contact = {
 
 		image:'/img/bio_photo.png',
 		bio:"<div class='paragraph'><div class='tabp'> Methods with Class is my on going project to bring some of the things I create to a wider audience.   </div>       <div class='tabp'>     I'm a mix of engineering and creativity with a passion for programming. I have skills in Objective-C, JavaScript, and now Ruby on Rails. This site is done in the Rails framework with some JavaScript integration for some features including the parallax scrolling on the Projects page.     </div>                                        <div class='tabp'>                        I am admittedly not a graphic designer and am not trying to be. I hope to partner with someone with vision or graphic skills who needs a web or mobile developer to bring their ideas to life.    </div>     </div><div class='paragraph'>    <div class='tabp'>    I am located near Washington, DC., <a href='mailto:chris@methodswithclass.com'> send us an email </a> with any questions or ideas.    </div>    </div>"
 	};
-
-	$scope.projects = apps;
-
-	$scope.contact = contact;
-
-	angular.element(document).ready(function () {
-
-		parallax.scroll(apps)
-
-		$("#projects").scroll(function () {
-			
-			parallax.scroll(apps);
-
-		});
-
-		$(window).resize(function () {
-
-			parallax.scroll(apps);
-		});
-	});
-
 
 }]);
