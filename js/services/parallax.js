84a4c2850fc84267c4be2cf49db2851278e65f06
methodswithclass <chris@methodswithclass.com>
2015-06-18 00:42:48 -0400

    parallax

--- js/services/parallax.js
+++ js/services/parallax.js
@@ -60,49 +60,47 @@ app.factory('parallax', function () {
 		return scrollFactor;
 	}
 
-	var scroll = function () {
+	return {
 
-		for (i in projects) {
+		scroll:function (projects) {
 
-			console.log(projects[i].id);
+			for (i in projects) {
 
-			space = $("#space" + projects[i].id);
-			image = $("#img" + projects[i].id);
+				console.log(projects[i].id);
 
-			console.log(image.height());
-		
-			if (whatDevice() == desktop) {
+				space = $("#space" + projects[i].id);
+				image = $("#img" + projects[i].id);
 
-				imgHeight = image.height();
-				imgOffset = image.offset().top;
-				elementHeight = space.height();
-				elementOffset = space.offset().top;
-				scrollHeight = $(window).height();
-				scrollWidth = $(window).width();
-
-				scrollFactor = getScrollFactor(scrollHeight, scrollWidth);
-
-				value = scrollFactor*(elementOffset + elementHeight)/scrollHeight*700 - 600;
-				
-				image.css("bottom", value);
-
-			}
-			else {
-
-				console.log("no browser");
-
-				value = -175;
+				console.log(image.height());
 			
-				image.css("bottom", value);	
+				if (whatDevice() == desktop) {
+
+					imgHeight = image.height();
+					imgOffset = image.offset().top;
+					elementHeight = space.height();
+					elementOffset = space.offset().top;
+					scrollHeight = $(window).height();
+					scrollWidth = $(window).width();
+
+					scrollFactor = getScrollFactor(scrollHeight, scrollWidth);
+
+					value = scrollFactor*(elementOffset + elementHeight)/scrollHeight*700 - 600;
+					
+					image.css("bottom", value);
+
+				}
+				else {
+
+					console.log("no browser");
+
+					value = -175;
+				
+					image.css("bottom", value);	
+				}
+
 			}
 
 		}
-
-	}
-
-	return {
-
-		scroll:scroll
 	}
 
 });
