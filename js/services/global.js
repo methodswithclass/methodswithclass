94b914d29ece166d8982a88736c9f01e439f4cb6
methodswithclass <chris@methodswithclass.com>
2015-06-19 10:55:04 -0400

    mobile

--- js/services/global.js
+++ js/services/global.js
@@ -6,6 +6,8 @@ app.factory('global', ['$sce', '$location', function($sce, $location) {
 
 	var isMobile = function () {
 
+		console.log($location.url());
+
 		if ($location.url() == "/mobile"){
 
 			return true;
