b05aa4e5ca74428d6154f3054be16ed62e5d1688
methodswithclass <chris@methodswithclass.com>
2015-06-20 11:44:10 -0400

    mobile

--- js/directives/blockinst.js
+++ js/directives/blockinst.js
@@ -4,6 +4,8 @@ app.directive("blockinst", function () {
 
 		link:function ($scope, element, attr) {
 
+			alert(attr.id);
+
 			if (attr.id == "blockInstnuplae") {
 
 				element.find(attr.id).text("click icon to play");
