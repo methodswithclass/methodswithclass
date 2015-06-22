304c2f2d6a2db391f9dbd7792ecb5f72a64ace03
methodswithclass <chris@methodswithclass.com>
2015-06-21 23:22:54 -0400

    scrollable

--- js/directives/scrollable.js
+++ js/directives/scrollable.js
@@ -1,70 +1,65 @@
 
 app.directive("scrollable", function () {
 
+	var i = 0;
+	var start;
+	var top;
+	var mouse0;
+	var mouse;
+	var vel0;
+	var vel;
+	var time0;
+	var time;
+	var interval;
+
+	var mu = 0.01;
+
+	var getMouse = function (e) {
+
+		return {x:e.pageX, y:e.pageY};
+	}
+
+	var getPos = function (touch) {
+
+		return 1.1*(touch.y - start.y);
+	}
+
+	var getVel = function (touch, touch0) {
+
+		return (getPos(touch) - getPos(touch0))/interval;
+	}
+
+	var getTime = function () {
+
+		return (new Date()).getTime();
+	}
+
+	var scroll = function (el, pos) {
+
+		el.css({'top': pos + top });
+	}
+
+	var momentum = function (vel0) {
+
+		var timer = setInterval(function () {
+
+			console.log("momentum");
+
+			scroll(vel0*interval);
+
+			vel0 *= (1-mu);
+
+			if (vel0 < 0.01) {
+
+				clearInterval(timer);
+			}
+
+		}, 50);
+	}
+
 	return {
 		restrict:'A',
 		link:function (scope, element, attr) {
-
-			var i = 0;
-			var start;
-			var top;
-			var mouse0;
-			var mouse;
-			var vel0;
-			var vel;
-			var time0;
-			var time;
-			var interval;
-			//var element = elem[0];
-
-
-			//console.log(typeof(element));
-
-
-			var mu = 0.01;
-
-			var getMouse = function (e) {
-
-				return {x:e.pageX, y:e.pageY};
-			}
-
-			var getPos = function (touch) {
-
-				return 1.1*(touch.y - start.y);
-			}
-
-			var getVel = function (touch, touch0) {
-
-				return (getPos(touch) - getPos(touch0))/interval;
-			}
-
-			var getTime = function () {
-
-				return (new Date()).getTime();
-			}
-
-			var scroll = function (pos) {
-
-				element.css({'top': pos + top });
-			}
-
-			var momentum = function (vel0) {
-
-				var timer = setInterval(function () {
-
-					console.log("momentum");
-
-					scroll(vel0*interval);
-
-					vel0 *= (1-mu);
-
-					if (vel0 < 0.01) {
-
-						clearInterval(timer);
-					}
-
-				}, 50);
-			}
 
 			element.on('touchstart', function (e) {
 
@@ -83,7 +78,7 @@ app.directive("scrollable", function () {
 				mouse = getMouse(e);
 				vel0 = getVel(mouse, mouse0);
 
-				scroll(getPos(mouse));
+				scroll(element, getPos(mouse));
 
 				mouse0 = mouse;
 				time0 = time;
