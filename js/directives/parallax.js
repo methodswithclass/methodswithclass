d480a23578037c1c75134795a204ba51309b4336
methodswithclass <chris@methodswithclass.com>
2015-06-24 16:35:38 -0400

    parallax

--- js/directives/parallax.js
+++ js/directives/parallax.js
@@ -4,7 +4,7 @@ app.directive('parallax', ['global', 'parallaxService', function (global, parall
 
 		//console.log("here");
 
-		var projects = scope.scrollItems;
+		var projects = scrollItems;
 
 		var scroll = function () {
 			for (i in projects) {
