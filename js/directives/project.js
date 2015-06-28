f9e69698fd8168bf61f452987554dd26ddc371ab
methodswithclass <chris@methodswithclass.com>
2015-06-27 22:30:15 -0400

    project

--- js/directives/project.js
+++ js/directives/project.js
@@ -77,21 +77,21 @@ app.directive('project', ['global', '$window', function (global, $window) {
 				bindResize(info);
 			}
 
-			$scope.projectsHeight = function (projects) {
+			// $scope.projectsHeight = function (projects) {
 
-				var sep = 0;
+			// 	var sep = 0;
 
-				for (i in projects) {
+			// 	for (i in projects) {
 
-					sep += $scope.sep(projects[i]);
-				}
+			// 		sep += $scope.sep(projects[i]);
+			// 	}
 
-				var result = projects.length*800 + sep;
+			// 	var result = projects.length*800 + sep;
 
-				console.log(result);
+			// 	console.log(result);
 
-				return result;
-			}
+			// 	return result;
+			// }
 
 			$scope.addText = function (id) {
 
@@ -105,7 +105,6 @@ app.directive('project', ['global', '$window', function (global, $window) {
 
 			$scope.clickImage = function (info) {
 
-				
 				$element = $("#sep" + info.id);
 				$space = $("#space" + info.id);
 
