dfe8226fa654eec1eb8ed94e1b720ea68908d765
methodswithclass <chris@methodswithclass.com>
2015-06-29 15:05:30 -0400

    scrollable

--- js/directives/scrollable.js
+++ js/directives/scrollable.js
@@ -220,20 +220,20 @@ app.directive("scrollable", ['global', '$window', 'notifications', function (glo
 
 		initPans();
 
-		notifications.registerChange("menu", function () {
+		notifications.register("menu", [self, function (self) {
 
-			for (i in self.enabled) {
-				self.enabled[i] = !self.enabled[i];
-				console.log(i + " " + self.enabled[i]);
-				console.log(self.scroll[ids[i]]);
-			}
+				for (i in self.enabled) {
+					self.enabled[i] = !self.enabled[i];
+					console.log(i + " " + self.enabled[i]);
+					console.log(self.scroll[ids[i]]);
+				}
 
-			self.scroll[ids[0]].set({enable:self.enabled[0]});
-			self.scroll[ids[1]].set({enable:self.enabled[1]});
+				self.scroll[ids[0]].set({enable:self.enabled[0]});
+				self.scroll[ids[1]].set({enable:self.enabled[1]});
 
-		});
+		}]);
 
-		notifications.change("menu");
+		notifications.call("menu");
 
 	}
 
--- js/services/notifications.js
+++ js/services/notifications.js
@@ -1,29 +1,60 @@
 app.factory("notifications", function () {
 
-	var self = this;
-
 	this.callback = {};
+	this.localVariables = {};
 
-	var registerChange = function (name, callback) {
+	var register = function (name, callback) {
 
 		console.log("registered " + name);
 
-		self.callback[name] = callback;
+		if (typeof callback == Array) {
+
+			var local = [];
+
+			for (i in callback) {
+
+				if (i < callback.length - 1) {
+					local[i] = callback[i];
+				}
+				else {
+					self.callback[name] = callback[i];
+				}
+
+				self.localVariables[name] = local;
+
+			}
+
+		}
+		else {
+
+			self.callback[name] = callback;
+			self.localVariables[name] = "none";
+		}
 
 	}
 
-	var change = function (name) {
+	var call = function (name) {
 
 		console.log("changed " + name);
-
+		
 		for(prop in self.callback) {
-			if (self.callback.hasOwnProperty(name)) self.callback[name]();
+			
+			if (self.callback.hasOwnProperty(name)) {
+				
+				if (self.localVariables[name] == "none") {
+					self.callback[name]();
+				}
+				else {
+					self.callback[name].apply(this, self.localVariables[name]);
+				}
+			}
 		}
+
 	}
 
 	return {
-		registerChange:registerChange,
-		change:change
+		register:register,
+		call:call
 	}
 
 });
