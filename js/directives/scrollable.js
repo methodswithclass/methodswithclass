0ea35bb94e72ace7fd59d2d569596f83854bcc28
methodswithclass <chris@methodswithclass.com>
2015-06-29 17:12:52 -0400

    scrollable

--- js/directives/scrollable.js
+++ js/directives/scrollable.js
@@ -25,7 +25,7 @@ app.directive("scrollable", ['global', '$window', 'notifications', function (glo
 
 	this.scroll = {};
 	var press;
-	var ids = [];
+	this.ids = [];
 	var element = {};
 	var i = 0;
 	var body;
@@ -236,20 +236,22 @@ app.directive("scrollable", ['global', '$window', 'notifications', function (glo
 
 		console.log("outside " + self.count);
 
-		// notifications.register("menu", [self, function () {
+		notifications.register("menu", function () {
 
-		// 		for (var i in this.enabled) {
-		// 			this.enabled[i] = !this.enabled[i];
-		// 			console.log(i + " " + this.enabled[i]);
-		// 			console.log(this.scroll[ids[i]]);
-		// 		}
+			for (var i in self.enabled) {
+				self.enabled[i] = !self.enabled[i];
+				console.log(i + " " + self.enabled[i]);
+				console.log(self.scroll[ids[i]]);
+			}
 
-		// 		this.scroll[ids[0]].set({enable:this.enabled[0]});
-		// 		this.scroll[ids[1]].set({enable:this.enabled[1]});
+			console.log("ids " + ids[0] + ids[1]);
 
-		// }]);
+			self.scroll[ids[0]].set({enable:self.enabled[0]});
+			self.scroll[ids[1]].set({enable:self.enabled[1]});
 
-		// notifications.call("menu");
+		});
+
+		notifications.call("menu");
 
 	}
 
