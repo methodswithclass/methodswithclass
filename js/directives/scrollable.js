d75c03255d65c1b0459ffefa503f2912a323e373
methodswithclass <chris@methodswithclass.com>
2015-06-29 23:17:57 -0400

    scrollable

--- js/directives/scrollable.js
+++ js/directives/scrollable.js
@@ -49,7 +49,7 @@ app.directive("scrollable", ['global', '$interval', 'notifications', 'con', func
 
 		var velocity;
 
-		if (Object.prototype.toString.call(vel) == '[object Object]' && vel.hasOwnPropery("velocityY")) {
+		if (Object.prototype.toString.call(vel) == '[object Object]' && vel.hasOwnProperty("velocityY")) {
 			velocity = Math.abs(100*vel.velocityY);
 			console.log("velocity");
 		}
