c30160c847073a2babec5d0a6eaa65cb0d8a777b
methodswithclass <chris@methodswithclass.com>
2015-07-01 19:12:32 -0400

    update url

--- js/directives/project.js
+++ js/directives/project.js
@@ -100,7 +100,7 @@ app.directive('project', ['global', '$window', '$location', function (global, $w
 
 				if (info.id == "nuplae") {
 
-					$location.absUrl("http://nuplae.methodswithclass.com");
+					$location.absUrl() == "http://nuplae.methodswithclass.com";
 				}
 			}
 		}
