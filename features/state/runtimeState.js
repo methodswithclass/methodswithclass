ed2fa8f7e640264b62fb09d04f52ea0afd45349a
methodswithclass <chris@methodswithclass.com>
2015-12-23 04:16:55 -0500

    update states

--- features/state/runtimeState.js
+++ features/state/runtimeState.js
@@ -3,98 +3,9 @@ stateModule.provider("runtime.state", function ($stateProvider) {
 
     var provider = {};
 
-    var checkingController = function () {
-
-        return ['validate.wrapper', 'con', 'events', '$document', '$location', '$stateParams', '$state', function (validate, con, events, $document, $location, $stateParams, $state) {
-
-                events.on("console", function () {
-
-                    //console.log("check console");
-
-                    return con.isRegistered();
-                });
-
-                // console.log("check inbound");
-
-                // var params = getParams($location.absUrl());
-
-                // console.log("params: " + params);
-
-                // var blog = "none";
-
-                // if (params.blog) {
-
-                //     if (data.resolveIndex(params.blog)) {
-
-                //         blog = blogs[params.blog].meta_data.name;
-
-                //     }
-                //     else {
-                //         blog = params.blog;
-                //     }
-
-                // }
-
-                // send.setup.save({name:"inbound", data:blog});
-
-                //$state.go("home");
-
-                angular.element($document).ready(function () {
-
-                    //console.log("document ready");
-
-                    con.register($("#consoleContainer"));
-
-                    con.attach();
-
-                });
-
-        }];
-
-    }
-
-    var blogController = function () {
-
-        return ['$scope', 'global', '$stateParams', 'data.service', function ($scope, g, $stateParams, data) {
-
-            console.log("name: " + $stateParams.name);
-
-            $scope.blog = data.getBlogByName($stateParams.name);
-
-            $scope.getContentUrl = function() {
-            
-                var view;
-
-                if (g.isMobile()) {
-
-                    view = "m.blog.html";
-                }
-                else {
-                    view = "d.blog.html";
-                }
-
-                return 'features/views/' + view;
-            }
-
-            $scope.myModel = {
-              Url: 'http://www.gentlephrasing.com/?b=' + $scope.blog.meta_data.name,
-              Name: $scope.blog.content.title.text,
-              ImageUrl: 'http://www.gentlephrasing.com/img/' + $scope.blog.meta_data.image + ".jpg"
-            };
-        
-
-        }];
-    }
-
     var states = [
     {
         name:"Default"
-    },
-    {
-        name:"checking",
-        url:"/checking",
-        templateUrl:'features/views/g.checking.html',
-        controller:checkingController()
     },
     {
         name:"Modal", 
@@ -134,12 +45,6 @@ stateModule.provider("runtime.state", function ($stateProvider) {
         name:"home",
         url:"/home",
         template:"<div ng-include='getContentUrl()'></div>"
-    },
-    {
-        name:"blog",
-        url:"/blog/:name",
-        template:"<div ng-include='getContentUrl()'></div>",
-        controller:blogController()
     },
     {
         name:"credits",
