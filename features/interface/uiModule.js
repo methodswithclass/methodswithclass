74d994a8c4753fdfb504dd825c6fcbc749fd463c
Christopher Polito <chris@polito.io>
2017-06-02 21:21:49 -0400

    update

--- features/interface/uiModule.js
+++ features/interface/uiModule.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 var uiModule = angular.module("uiModule", ['sharedModule', 'dataModule', 'ui.router', 'stateModule' /*, 'parallaxModule'*/]);
-=======
-var uiModule = angular.module("uiModule", ['sharedModule', 'dataModule', 'ngRoute', 'ui.router', 'stateModule', 'parallaxModule']);
->>>>>>> 38d6b12f3c7c63afd1a9a0b2c39c3720401601ac
