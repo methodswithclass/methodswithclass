6f3cb15b5f89a235ccf04654ec628298d18a71e8
Christopher Polito <chris@polito.io>
2017-06-03 21:53:57 -0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,11 @@ concat = require('gulp-concat'),
 cache = require('gulp-cache'),
 del = require('del'),
 inject = require('gulp-inject'),
-angularFilesort = require('gulp-angular-filesort');
+angularFilesort = require('gulp-angular-filesort'),
+order = require("order"),
+filter = require("gulp-filter"),
+merge = require("merge-stream"),
+mainBowerFiles = require("main-bower-files");
 
 
 gulp.task('serve', function() {
@@ -23,36 +27,44 @@ gulp.task('serve', function() {
 	}));
 });
 
-gulp.task('watch', function() {
+// gulp.task('watch', function() {
 
-	// Create LiveReload server
-	livereload.listen();
+// 	// Create LiveReload server
+// 	livereload.listen();
 
-	// Watch any files in dist/, reload on change
-	gulp.watch(['src/features/**/*.js', "src/css/**/*.css", "src/**/*.html"]).on('change', livereload.changed);
+// 	// Watch any files in dist/, reload on change
+// 	gulp.watch(['src/features/**/*.js', "src/css/**/*.css", "src/**/*.html"]).on('change', livereload.changed);
 
-	// Watch .scss files
-	gulp.watch('src/css/**/*.css', ['styles']);
+// 	// Watch .scss files
+// 	gulp.watch('src/css/**/*.css', ['styles']);
 
-	// Watch .js files
-	gulp.watch('src/features/**/*.js', ['scripts']);
+// 	// Watch .js files
+// 	gulp.watch('src/features/**/*.js', ['scripts']);
 
-	// Watch image files
-	gulp.watch('src/img/**/*', ['images']);
+// 	// Watch image files
+// 	gulp.watch('src/img/**/*', ['images']);
 
-});
+// });
 
 gulp.task('styles', function() {
 	return gulp.src('src/assets/css/**/*.css', { style: 'expanded' })
 	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+	// .pipe(concat("styles.css"))
+	// .pipe(rename({suffix: '.min'}))
+	// .pipe(uglify())
 	.pipe(gulp.dest('dist/assets/css'));
 });
 
-gulp.task('scripts', function() {
-	return gulp.src('src/features/**/*.js')
+gulp.task('scripts', ['vendor'], function() {
+	return gulp.src([
+	            "src/features/data/dataModule.js",
+	            "src/features/state/stateModule.js",
+	            "src/features/interface/uiModule.js",
+	            "src/features/**/*.js",
+	            "src/features/app/app.js"
+	            ])
 	// .pipe(jshint('.jshintrc'))
 	// .pipe(jshint.reporter('default'))
-	.pipe(angularFilesort())
 	.pipe(concat('main.js'))
 	// .pipe(rename({suffix: '.min'}))
 	// .pipe(uglify())
@@ -61,8 +73,21 @@ gulp.task('scripts', function() {
 
 gulp.task("vendor", function () {
 
-	return gulp.src(["bower_components/**/*.js", "bower_components/**/*.css"])
-	.pipe(gulp.dest("dist/bower_components"))
+	var js = gulp.src(mainBowerFiles(), { base: __dirname + '/bower_components' })
+	.pipe(filter("**/*.js"))
+	.pipe(concat("vendor.js"))
+	// .pipe(rename({suffix: '.min'}))
+	// .pipe(uglify())
+	.pipe(gulp.dest("dist/assets/js"));
+
+	var css = gulp.src(mainBowerFiles())
+	.pipe(filter("**/*.css"))
+	.pipe(concat("vendor.css"))
+	// .pipe(rename({suffix: '.min'}))
+	// .pipe(uglify())
+	.pipe(gulp.dest("dist/assets/css"));
+
+	return merge(js, css);
 });
 
 gulp.task("html", function () {
@@ -89,14 +114,16 @@ gulp.task("misc", function () {
 	.pipe(gulp.dest("dist"));
 })
 
-gulp.task('index', ["styles", "scripts", 'vendor', 'html', "fonts", "images", "misc"], function () {
+gulp.task('index', ["styles", "scripts", 'html', "fonts", "images", "misc"], function () {
 
 	// It's not necessary to read the files (will speed up things), we're only after their paths: 
-	var sources = gulp.src(['./dist/assets/**/*.js', './dist/assets/**/*.css'], {read: false});
+	var important = gulp.src('dist/assets/js/vendor.js', {read: false});
+	var standard = gulp.src(["dist/assets/js/main.js", 'dist/assets/**/*.css'], {read:false});
 
-	return gulp.src('./src/index.html')
-	.pipe(inject(sources))
-	.pipe(gulp.dest('./dist'));
+	return gulp.src('src/index.html')
+	.pipe(inject(important, {ignorePath:"dist", starttag: '<!-- inject:head:{{ext}} -->'}))
+	.pipe(inject(standard, {ignorePath:"dist"}))
+	.pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function() {
